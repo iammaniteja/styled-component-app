762a096e001f9f113cbd4a0591e039fcf78ea8cb
maniteja <murarima@mail.uc.edu>
2021-09-23 00:18:00 -0400

    Styled example form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Button = styled.button `
+color: white;
+border-radius: 5px;
+margin: 5px;
+background-color: black;
+`
+
+const Input = styled.input `
+border: 2px solid;
+border-radius: 3px;
+margin: 5px;
+padding: 5px;
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple Form</h1>
+      <Input placeholder = "Name"/>
+      <Input placeholder = "Password" type = "password"/>
+      <Button>Login</Button>
     </div>
   );
 }
